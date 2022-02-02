cfce4e58f4a2a808db207f5be2c41551f492ce16
Maxwell Woodruff <mmwoodruff@cps.edu>
2022-02-02 13:05:18 -0600

    Added Gravity

---
+++ Avalon/Assets/Playermovement.cs
@@ -0,0 +1,20 @@
+
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+   public Rigidbody2D rb;
+   
+    void Start()
+    {
+     Debug.Log("Hello,God");
+       
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
