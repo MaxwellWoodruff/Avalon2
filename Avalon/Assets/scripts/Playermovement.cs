32b20bd6182235ae98324d30862902b3007189e5
Maxwell Woodruff <mmwoodruff@cps.edu>
2022-02-09 15:00:38 -0600

    Better Player Movement

--- Avalon/Assets/scripts/Playermovement.cs
+++ Avalon/Assets/scripts/Playermovement.cs
@@ -3,9 +3,10 @@ using System.Collections;
 public class Playermovement : MonoBehaviour
 
 {
-   public Rigidbody2D rb;
-   public float speedX;
-   public float speedY;
+   //public Rigidbody2D rb;
+   //public float speedX;
+   //public float speedY;
+   public Vector2 speed = new Vector2(50,50);
    
     void Start()
     {
@@ -18,30 +19,35 @@ public class Playermovement : MonoBehaviour
     
     void Update()
     {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(speed.x*inputX, speed.y*inputY,0);
+        movement*= Time.deltaTime;
+        transform.Translate(movement);
       // Debug.Log("this is the start");
-       if(Input.GetKeyDown(KeyCode.A))
-       {
-           transform.position = transform.position + 
-            new Vector3(-1,0,0);
-        Debug.Log("The A key was pressed");   
-       }
-       if(Input.GetKeyDown(KeyCode.S))
-       {
-            transform.position = transform.position + 
-            new Vector3(0,-1,0);
-           Debug.Log("The S key was pressed");
-       }
-       if(Input.GetKeyDown(KeyCode.W))
-       {
-           transform.position = transform.position + 
-            new Vector3(0,1,0);
-        Debug.Log("The W key was pressed");   
-       }
-       if(Input.GetKeyUp(KeyCode.D))
-       {
-           transform.position = transform.position + 
-            new Vector3(1,0,0);
-        Debug.Log("The D key was pressed");   
-       }
+      // if(Input.GetKeyDown(KeyCode.A))
+     // {
+      //     transform.position = transform.position + 
+       //     new Vector3(-1,0,0);
+      //  Debug.Log("The A key was pressed");   
+      // }
+      // if(Input.GetKeyDown(KeyCode.S))
+       //{
+       //     transform.position = transform.position + 
+       //     new Vector3(0,-1,0);
+       //    Debug.Log("The S key was pressed");
+      // }
+      // if(Input.GetKeyDown(KeyCode.W))
+      // {
+      //     transform.position = transform.position + 
+       //     new Vector3(0,1,0);
+      //  Debug.Log("The W key was pressed");   
+     //  }
+       //if(Input.GetKeyUp(KeyCode.D))
+      // {
+      //     transform.position = transform.position + 
+       //     new Vector3(1,0,0);
+       // Debug.Log("The D key was pressed");   
+      // }
     } 
 }
