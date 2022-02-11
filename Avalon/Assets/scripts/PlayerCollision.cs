727aadcdb45cc3cbd5887233c245f0b820e33ab0
Maxwell Woodruff <mmwoodruff@cps.edu>
2022-02-11 09:40:43 -0600

    included smooth movement

--- Avalon/Assets/scripts/PlayerCollision.cs
+++ Avalon/Assets/scripts/PlayerCollision.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
 
+
 public class PlayerCollision : MonoBehaviour
 {
+public float dumbass;
+
+
     void OnCollisionEnter2D(Collision2D col)
     {
         Debug.Log("we hit");
--- Avalon/Assets/scripts/Playermovement.cs
+++ Avalon/Assets/scripts/Playermovement.cs
@@ -3,51 +3,40 @@ using System.Collections;
 public class Playermovement : MonoBehaviour
 
 {
-   //public Rigidbody2D rb;
-   //public float speedX;
-   //public float speedY;
-   public Vector2 speed = new Vector2(50,50);
+   public Rigidbody2D rb;
+   
+   public float speed;
+
+   public Animator animator;
    
     void Start()
     {
-        
-     //Debug.Log("Hello,God");
-       
-        
+    rb = GetComponent<Rigidbody2D>();
+    animator = gameObject.GetComponent<Animator>();
     }
 
     
     void Update()
     {
-        float inputX = Input.GetAxis("Horizontal");
-        float inputY = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(speed.x*inputX, speed.y*inputY,0);
-        movement*= Time.deltaTime;
-        transform.Translate(movement);
-      // Debug.Log("this is the start");
-      // if(Input.GetKeyDown(KeyCode.A))
-     // {
-      //     transform.position = transform.position + 
-       //     new Vector3(-1,0,0);
-      //  Debug.Log("The A key was pressed");   
-      // }
-      // if(Input.GetKeyDown(KeyCode.S))
-       //{
-       //     transform.position = transform.position + 
-       //     new Vector3(0,-1,0);
-       //    Debug.Log("The S key was pressed");
-      // }
-      // if(Input.GetKeyDown(KeyCode.W))
-      // {
-      //     transform.position = transform.position + 
-       //     new Vector3(0,1,0);
-      //  Debug.Log("The W key was pressed");   
-     //  }
-       //if(Input.GetKeyUp(KeyCode.D))
-      // {
-      //     transform.position = transform.position + 
-       //     new Vector3(1,0,0);
-       // Debug.Log("The D key was pressed");   
-      // }
-    } 
+       animator.SetFloat("speed", moveHorizontal*speed);
+       if(Input.GetKeyDown(KeyCode.A))
+       {
+        Debug.Log("The A key was pressed");   
+       }
+       if(Input.GetKeyUp(KeyCode.S))
+      {    
+          Debug.Log("The S key was pressed");
+      }
+       if(Input.GetKeyDown(KeyCode.W))
+       {  
+        Debug.Log("The W key was pressed");   
+       }
+       if(Input.GetKeyDown(KeyCode.D))
+       {  
+        Debug.Log("The D key was pressed");   
+       }
+      float moveHorizontal = Input.GetAxis("Horizontal");
+      float moveVertical = Input.GetAxis("Vertical");
+      rb.velocity = new Vector2 (moveHorizontal*speed, moveVertical*speed);
+    }     
 }
