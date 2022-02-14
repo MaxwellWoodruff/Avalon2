e853cb8b592c656de1e11d802cf09b3b27af392c
Maxwell Woodruff <mmwoodruff@cps.edu>
2022-02-14 11:19:19 -0600

    Left and right animations work

---
+++ Avalon/Assets/scripts/PlayerAttack.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+
+    public Animator animator;
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKeyDown(KeyCode.T));
+        //{
+          //  Attack();
+        //}
+        
+    }
+
+    //void Attack()
+    //{
+      //  animator.SetTrigger("Attack");
+    //}
+}
