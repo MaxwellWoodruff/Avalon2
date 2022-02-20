bd7e8683162be7a4a97108fdc4eee0c521dcb505
MaxwellWoodruff <40668505+MaxwellWoodruff@users.noreply.github.com>
2022-02-20 15:45:35 -0600

    Crouching Animation Works

--- Avalon/Avalon/Assets/scripts/Playermovement.cs
+++ Avalon/Avalon/Assets/scripts/Playermovement.cs
@@ -10,14 +10,10 @@ public class Playermovement : MonoBehaviour
    public Animator animator;
    
    public float moveHorizontal;
-
-   public float attackrecovery;
-   
     void Start()
     {
     rb = GetComponent<Rigidbody2D>();
     animator = gameObject.GetComponent<Animator>();
-    attackrecovery=0;
     }
 
     
@@ -26,17 +22,8 @@ public class Playermovement : MonoBehaviour
         
         float moveHorizontal = Input.GetAxis("Horizontal");
       float moveVertical = Input.GetAxis("Vertical");
-        if(attackrecovery>0)
-      {
-        attackrecovery--;
-        moveHorizontal=0;
-      }
       rb.velocity = new Vector2 (moveHorizontal*speed, moveVertical*speed);
-        if(Input.GetKeyUp(KeyCode.T))
-       {  
-        Debug.Log("Attacking");
-       attackrecovery = 91;  
-       }
+       
        animator.SetFloat("speed",Mathf.Abs (moveHorizontal*speed));
        if(Input.GetKeyDown(KeyCode.A))
        {
@@ -44,11 +31,11 @@ public class Playermovement : MonoBehaviour
        }
        if(Input.GetKeyUp(KeyCode.S))
       {    
-       //   Debug.Log("Crouching");
+          Debug.Log("Crouching");
       }
        if(Input.GetKeyDown(KeyCode.W))
        {  
-        //Debug.Log("Jumping");   
+        Debug.Log("Jumping");   
        }
        if(Input.GetKeyDown(KeyCode.D))
        {  
