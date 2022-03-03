4afcf9a71980cff8d11be95cdb6f2647486b434e
Maxwell Woodruff <mmwoodruff@cps.edu>
2022-03-03 11:26:32 -0600

    Added second player, Fixed Jumping boolean animation

---
+++ Avalon4/Assets/scripts/Playermovement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+public class Playermovement : MonoBehaviour
+
+{
+   public Rigidbody2D rb;
+
+   public float speed;
+
+   public Animator animator;
+   
+   public float moveHorizontal;
+    void Start()
+    {
+    rb = GetComponent<Rigidbody2D>();
+    animator = gameObject.GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+      float moveVertical = Input.GetAxis("Vertical");
+      rb.velocity = new Vector2 (moveHorizontal*speed, moveVertical*speed);
+       
+       animator.SetFloat("speed",Mathf.Abs (moveHorizontal*speed));
+       if(Input.GetKeyDown(KeyCode.A))
+       {
+        Debug.Log("Running to right"); 
+       }
+       if(Input.GetKeyUp(KeyCode.S))
+      {    
+          Debug.Log("Crouching");
+      }
+       if(Input.GetKeyDown(KeyCode.W))
+       {  
+        Debug.Log("Jumping");   
+       }
+       if(Input.GetKeyDown(KeyCode.D))
+       {  
+        Debug.Log("Righting to the left");   
+       }
+    }     
+}
